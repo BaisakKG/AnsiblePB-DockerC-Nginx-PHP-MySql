bb3b15c21bc7f86dca2285be1850144581d7fa0b
Baisak Sagynov <baisak40@gmail.com>
2024-10-05 00:14:27 +0600

    Initiall commit

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+USE crud_db;
+CREATE TABLE `userdetails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
